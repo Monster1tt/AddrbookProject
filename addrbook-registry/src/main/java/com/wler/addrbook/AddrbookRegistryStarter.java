09ec0f6b3f3a066880aa16c56f44d628146bacfe
Monster <monster1tt@163.com>
2021-04-16 15:09:33 +0800

    创建 addrbook-registry 项目

--- addrbook-registry/src/main/java/com/wler/addrbook/AddrbookRegistryStarter.java
+++ addrbook-registry/src/main/java/com/wler/addrbook/AddrbookRegistryStarter.java
@@ -2,10 +2,12 @@ package com.wler.addrbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
+@SpringBootApplication
 public class AddrbookRegistryStarter {
 	public static void main(String[] args) {
-		SpringApplication.run(AddrbookRegistryStarter.class,args);
+		SpringApplication.run(AddrbookRegistryStarter.class, args);
 	}
 }
